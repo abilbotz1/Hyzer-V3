93fb459988e1e47d5479473a9e987205efca1e38
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-07-09 11:57:25 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,7 +3,7 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
-FN: Zxyuu Romm
+FN: RielGans🗿
 item.ORG: ytta!
 item1.TEL;waid=6281252232882:6282146218274@s.whatsapp.net
 item1.X-ABLabel:Sibuk 
@@ -12,19 +12,19 @@ item2.X-ABLabel:Email
 item3.ADR:;;🇮🇩Indonesia;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:📍 LOCATION
-item4.URL:https://github.com/
+item4.URL:https://github.com/Rieltod
 item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
     m.chat,
     { 
         contacts: { 
-            displayName: 'OWNER GW', 
+            displayName: 'OWNER AINK🗿', 
             contacts: [{ vcard }] 
         }
     }
 )
-await conn.send2But(m.chat, "*----------[ OWNER ]----------*\n\n• https://wa.me/6281252232882 (anton)\n• https://wa.me/6282146218274 (Rielgans)\n\n📮Note:\n》 Chat Main' Ga bakal di tanggapi\n》 Dilarang Telpon Or vc\n\n_Dah Segitu ae_", 'Lemon', 'Group Lemonbotz', '.gcbot', 'Menunya🤖', '.menu', sentMsg)}
+await conn.send2But(m.chat, "*----------[ OWNER ]----------*\n\n• https://wa.me/6282146218274 (RielXzy)\n• https://wa.me/6283112471048(botnya riel)\n\n📮Note:\n》 Chat Main' Ga bakal di tanggapi\n》 Dilarang Telpon Or vc\n\n_Dah Segitu ae_", 'RieXzy', 'Group Rielbotzbotz', '.gcbot', 'Menunya🤖', '.menu', sentMsg)}
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
